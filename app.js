e33754293de4038c91417fe31723f0eaca39b811
hasibulislam999 <hasib143sl@gmail.com>
2022-09-27 21:28:37 +0600

    driver code added

--- app.js
+++ app.js
@@ -7,21 +7,15 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 
-
 //routes
-const productRoute = require('./routes/product.route');
+const productRoute = require("./routes/product.route");
 const brandRoute = require("./routes/brand.route");
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
 
-
-app.use('/api/v1/product', productRoute);
+app.use("/api/v1/product", productRoute);
 app.use("/api/v1/brand", brandRoute);
 
 module.exports = app;
-
-
-
-

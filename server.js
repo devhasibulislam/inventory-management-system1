2d5acd70db5fdb7c26bc4eb6d9ab7a578a192128
hasibulislam999 <hasib143sl@gmail.com>
2022-09-28 11:18:10 +0600

    server modified

--- server.js
+++ server.js
@@ -5,13 +5,21 @@ const colors = require("colors");
 const app = require("./app");
 
 // database connection
-mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
-  console.log(colors.green.bold(`Database connection is successful 🛢`));
-});
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    dbName: "inventory-management-system",
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log(
+      colors.bgGreen.bold(`Database connection is successful`)
+    );
+  });
 
 // server
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(colors.yellow.bold(`App is running on port ${port}`));
+  console.log(colors.bgYellow.bold(`App is running on port ${port}`));
 });

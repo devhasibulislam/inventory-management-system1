68f014d3185548ed0eccd69d1854c2428a4a1a6b
hasibulislam999 <hasib143sl@gmail.com>
2022-09-28 18:05:52 +0600

    product service modified

--- services/product.services.js
+++ services/product.services.js
@@ -1,16 +1,15 @@
 const Product = require("../models/Product");
 
 exports.getProductsService = async (filters, queries) => {
-
   const products = await Product.find(filters)
     .skip(queries.skip)
     .limit(queries.limit)
     .select(queries.fields)
-    .sort(queries.sortBy)
+    .sort(queries.sortBy);
 
-  const total = await Product.countDocuments(filters)
-  const page = Math.ceil(totalProducts/queries.limit)
-  return {total,page,products};
+  const total = await Product.countDocuments(filters);
+  const page = Math.ceil(totalProducts / queries.limit);
+  return { total, page, products };
 };
 
 exports.createProductService = async (data) => {

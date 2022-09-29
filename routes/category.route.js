39f266fd1c6f3753eeb2b1e3bc17720582e78110
hasibulislam999 <hasib143sl@gmail.com>
2022-09-29 12:22:28 +0600

    category route added

---
+++ routes/category.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const categoryController = require("../controllers/category.controller");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(categoryController.getCategories)
+  .post(categoryController.createCategory);
+
+router
+  .route("/:id")
+  .get(categoryController.getCategoryById)
+  .patch(categoryController.updateCategoryById);
+
+module.exports = router;
